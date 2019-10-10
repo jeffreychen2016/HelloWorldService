903355c6fa171077ba9df2b3c4a1fd29ea837c33
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-10-10 16:27:44 -0500

    create sample WCF service and host

---
+++ HelloWorldHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace HelloWorldHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(HelloWorldService.HelloWorldService)))
+            {
+                host.Open();
+                Console.WriteLine("Service is running at port 8733...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
